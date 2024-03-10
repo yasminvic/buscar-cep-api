80811be0bb1e0f5f73bf8e1ed61bbd86e14bf5fa
Yasmin <yasminalvesdesouza27@gmail.com>
2024-03-10 20:42:37 -0300

    Feat - Autenticação Finalizada

--- SlnBuscarCEP/src/Application.API/Program.cs
+++ SlnBuscarCEP/src/Application.API/Program.cs
@@ -7,6 +7,7 @@ using Infra.Data.Repository.Repository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Refit;
 using System.Text;
 
@@ -22,7 +23,33 @@ builder.Services.AddDbContext<SQLServerContext>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//Config Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Buscar CEP" });
+
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "Autenticação TokenJWT",
+        Description = "Entre com seu TokenJWT para acessar a API",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { securityScheme, new string[] {} }
+    });
+});
 
 
 //Dependency Injection
